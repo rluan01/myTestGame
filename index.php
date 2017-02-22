b5ece1774e72291ff67f6ce2d045387eba6eeb57
rluan01 <digao.csl@gmail.com>
2017-02-22 19:09:38 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>rluan</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/css.css">
+		<script src="js/myjs.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row"> <!-- main screen where all will will happen  |||  reponsive -->
+				<div class="col-md-12">
+					<!-- main message -->
+					<h1>WELCOME</h1>
+					<div class="row">
+						<div class="col-md-9">
+							<!-- here is the game -->
+							<div class="backGame">
+								<p id="idMsg" class="msg" onclick="start()">Click to start the game idiot</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<!-- this is the chat -->
+							<div class="chat" >
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
